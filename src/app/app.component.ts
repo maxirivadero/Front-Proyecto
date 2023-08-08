35202299796acc23e259f255f08f8354ec824687
maxirivadero <50490597+maxirivadero@users.noreply.github.com>
2023-08-08 18:14:57 -0300

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+  
+export class AppComponent implements OnInit{
+  title = 'santex-academy';
+  constructor(private http: HttpClient) {
+    
+  }
+
+  ngOnInit(): void {
+    //this.http.get("http://localhost:4001".subscribe());
+  }
+
+}
+
